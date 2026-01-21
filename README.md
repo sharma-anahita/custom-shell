781348c153359cc1669ecfc31c4f0d21e34efa1c
Anahita Sharma <sharma.anahita.as@gmail.com>
2026-01-21 18:21:58 +0530

    Add initial README and main shell implementation

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # custom-shell
+this is in C
+for windows only right now
+need for all
+get a way to recognise which system it is
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+//what this shell is going to support
+//execute the inbuilt commands
+//execute the external commands
+//we will also manage path through our shell
+//and error handling will also be done
+//we will first have to create a loop that will keep on running until the user wants to exit
