267e86ca0fbd431c2c6341ecbf1f9b11f87f2301
Anahita Sharma <sharma.anahita.as@gmail.com>
2026-01-23 01:20:05 +0530

    Add .gitignore, update shelly.h with new built-in commands, and add binary files

--- shelly.h
+++ shelly.h
@@ -1,25 +1,29 @@
+#ifndef SHELLY_H
+#define SHELLY_H
+
 #include <stdio.h>
-#include <stdbool.h>  
 #include <stdlib.h>
-#include <unistd.h>   // getcwd
+#include <unistd.h>
 #include <string.h>
 
 #define MAX_INPUT_SIZE 1024
 
-char** input_parser(char* input);
-int shell_builtin_execute(char** args, char** env, char* inputDirectroy);
+char **input_parser(char *input);
+void free_tokens(char **tokens);
 
-int my_strcmp(const char* str1, const char* str2);
+int my_strcmp(const char *str1, const char *str2);
 
+/* builtins */
+void command_cd(char **args, char *inputDirectory);
+void command_pwd(char **args);
+void command_which(char **args, char **env);
+void command_echo(char **args);
+void command_help(char **args);
+void command_env(char **args, char **env);
+void command_set(char **args, char **env);
+void command_unset(char **args, char **env);
 
-int command_cd(char ** args,char* inputDirectory);
-int command_pwd(char ** args);;
-int command_which(char ** args, char** env);
-int command_echo(char** args);
-int command_help(char** args); 
-int command_env(char** args, char** env);
-int command_set(char** args, char** env);   
-int command_unset(char** args, char** env);
+/* external */
 void command_external(char **args, char **env);
 
-void free_tokens(char **tokens);
+#endif
