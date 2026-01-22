5a308cac58285941432ea4038463a09ac93dc61f
Anahita Sharma <sharma.anahita.as@gmail.com>
2026-01-22 09:54:06 +0530

    Implement built-in command execution and update header file

--- main.c
+++ main.c
@@ -7,12 +7,62 @@
 //we will also manage path through our shell
 //and error handling will also be done
 //we will first have to create a loop that will keep on running until the user wants to exit
+int shell_builtin_execute(char** args, char** env, char* inputDirectroy){
+    //this function will execute the inbuilt commands
+    //cd, pwd,whihch, echo,exit,help,env,set,unset,etc
+    if(stcmp(args[0],"cd") == 0){
+        command_cd(args,inputDirectroy);
+        
+    }
+    else if(stcmp(args[0],'pwd')==0){
+        command_pwd(args);
+    }
+    else if(stcmp(args[0],'which')==0){
+        command_which(args,env);
+        //exlpain which to me
+        //The which command in Unix-like operating systems is used to locate the executable file associated with a given command by searching through the directories listed in the user's PATH environment variable.
+        //does user need to provide env here?
+        //yes because which needs to search through the PATH variable
 
+    }
+    else if(stcmp(args[0],'echo ')==0){
+        command_echo(args);
+    }
+    else if(stcmp(args[0],'help')==0){
+        command_help(args);
+        //does help need args?
+        //yes because help can provide help for specific commands
+        //env?
+        //no
+        //why
+        //because help does not need to search through environment variables
+    }
+    else if(stcmp(args[0],'exit')==0 || stcmp(args[0],'quit')==0){
+        command_exit(args);
+    }
+    else if(stcmp(args[0],'env')==0){
+        command_env(args,env);
+    }
+    else if(stcmp(args[0],'set')==0){
+        command_set(args,env);
+    }
+    else if(stcmp(args[0],'unset')==0){
+        command_unset(args,env);
+    }
+    else{
+        //external command
+        //handle this later
+        command_external(args,env);
+    }
+    
+}
 void shell_loop(char** env){
     //1. setup  input and loop
     char* input = NULL;
     size_t inputLen = 0;
     char** args;
+    char * inputDirectory = getcwd(NULL,0); //get the current working directory
+    
     while(1){
         printf("(shelly)> ");
         if(getline(&input,&inputLen,stdin) == -1){
@@ -31,8 +81,11 @@ void shell_loop(char** env){
         //to execute the commands we will create a new function 
         //first we will check if the command is inbuilt or external
         //if inbuilt we will execute it directly
-        //right now we will make the inbuild command function stub
-        
+        //right now we will make the inbuild command function 
+        shell_builtin_execute(args,env,inputDirectory);
+        if(args[0]!=NULL){
+            shell_builtin_execute(args,env,inputDirectory);
+        }
     }
 }
 int main(int argc, char** argv,char** env){
@@ -41,4 +94,5 @@ int main(int argc, char** argv,char** env){
     (void)argv; // we don't need them
     printf("Welcome to this simple shell!\n");
     shell_loop(env);
+
 }
--- shelly.h
+++ shelly.h
@@ -5,3 +5,14 @@
 #define MAX_INPUT_SIZE 1024
 
 char** input_parser(char* input);
+int shell_builtin_execute(char** args, char** env, char* inputDirectroy);
+
+int command_cd(char ** args,char* inputDirectory);
+int command_pwd(char ** args);;
+int command_which(char ** args, char** env);
+int command_echo(char** args);
+int command_help(char** args);
+int command_exit(char** args);
+int command_env(char** args, char** env);
+int command_set(char** args, char** env);   
+int command_unset(char** args, char** env);
